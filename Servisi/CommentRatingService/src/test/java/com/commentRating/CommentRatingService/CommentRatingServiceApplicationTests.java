79cfbe72b0052c021c1b5baf514e40d6b09afc9f
GoranKuljanin <goku.kuljanin@gmail.com>
2020-05-21 16:52:01 +0200

    Servisi i docker-compose

---
+++ Servisi/CommentRatingService/src/test/java/com/commentRating/CommentRatingService/CommentRatingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.commentRating.CommentRatingService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommentRatingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
