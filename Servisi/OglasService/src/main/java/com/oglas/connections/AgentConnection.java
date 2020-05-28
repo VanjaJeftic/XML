a73dfefd5a3c8f7727b51895024368188be52eb4
VanjaJeftic <vanjajeftic97@gmail.com>
2020-05-28 20:58:24 +0200

    ispravka greske

--- Servisi/OglasService/src/main/java/com/oglas/connections/AgentConnection.java
+++ Servisi/OglasService/src/main/java/com/oglas/connections/AgentConnection.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "agent") //samo proveriti da li se ovo vozilo odnosi na naziv u bazi za Vozilo service
+@FeignClient(name = "eureka-authorization-service") //samo proveriti da li se ovo vozilo odnosi na naziv u bazi za Vozilo service
 public interface AgentConnection {
 
     @GetMapping("/verify/{agent_id}")
