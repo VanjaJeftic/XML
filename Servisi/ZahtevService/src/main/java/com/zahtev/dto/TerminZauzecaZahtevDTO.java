6479ad6c1f9062ac45febeb48bf6acafb6009f1e
GoranKuljanin <goku.kuljanin@gmail.com>
2020-06-03 16:50:16 +0200

    Sredjeno zauzece termina za vozilo

--- Servisi/ZahtevService/src/main/java/com/zahtev/dto/TerminZauzecaZahtevDTO.java
+++ Servisi/ZahtevService/src/main/java/com/zahtev/dto/TerminZauzecaZahtevDTO.java
@@ -1,11 +1,14 @@
 package com.zahtev.dto;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 public class TerminZauzecaZahtevDTO {
 
 	private List<Long> oglasi = new ArrayList<>();
+	private LocalDateTime preuzimanje;
+	private LocalDateTime povratak;
 	
 	public TerminZauzecaZahtevDTO() {
 		// TODO Auto-generated constructor stub
@@ -17,5 +20,20 @@ public class TerminZauzecaZahtevDTO {
 
 	public void setOglasi(List<Long> oglasi) {
 		this.oglasi = oglasi;
+	}
+	public LocalDateTime getPreuzimanje() {
+		return preuzimanje;
+	}
+
+	public void setPreuzimanje(LocalDateTime preuzimanje) {
+		this.preuzimanje = preuzimanje;
+	}
+
+	public LocalDateTime getPovratak() {
+		return povratak;
+	}
+
+	public void setPovratak(LocalDateTime povratak) {
+		this.povratak = povratak;
 	}
 }
